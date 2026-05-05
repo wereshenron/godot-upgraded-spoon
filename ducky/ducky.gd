075e63e6b28bdea8f3f0fff2cf59d9312a9e262f
Austin Duling <austinduling@gmail.com>
2026-05-05 15:45:08 -0400

    ducky changes

--- ducky/ducky.gd
+++ ducky/ducky.gd
@@ -5,8 +5,3 @@ extends RigidBody3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	angular_velocity = Vector3(randf(), randf(), randf())
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
